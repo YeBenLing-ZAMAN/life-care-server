f91e4104ada9092c924300a93bb3e0443a38506e
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-05-15 12:28:34 +0600

    available service filter section add

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+/* middleware */
+app.use(cors());
+app.use(express.json());
+
+/* mongoDB  */
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hd4la.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('doctor_bhai').collection('services');
+        const bookingCollection = client.db('doctor_bhai').collection('booking');
+
+        /***
+         * API Naming Convestion
+         * app.get('/booking') // get all bookings in this collection. get one more than by any filter
+         * app.get('/booking/:id')// get a specific booking 
+         * app.post('/booking') // add a new booking
+         * app.patch('/booking/:id') //one item update on your DB
+         * app.delete('/booking/:id') //one item delete on your DB
+         */
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = await serviceCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/available', async (req, res) => {
+            const date = req.query.date || 'May 15, 2022';
+            // step-01: GET ALL SERVICES 
+            const services = await serviceCollection.find().toArray();
+            // res.send(services);
+
+            //step-02: get the bookig of that day
+            const query = { date: date };
+            const bookings = await bookingCollection.find(query).toArray();
+            // res.send(booking);
+
+            // step-03: for each service, find bookings for that service
+            services.forEach(service => {
+                const serviceBooking = bookings.filter(b => b.treatment === service.name);
+                const booked = serviceBooking.map(s => s.slot);
+                // service.booked = booked; // for checking ;
+                const available = service.slots.filter(s => !booked.includes(s));
+                service.available = available;
+
+            })
+            res.send(services);
+        })
+
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            // console.log(booking);
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
+            const result = await bookingCollection.insertOne(booking);
+            return res.send({ success: true, result });
+        })
+
+
+    } finally {
+
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello Doctor bhai');
+})
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
