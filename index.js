46ed58d4f4120b99d26c6854d918e3b256500915
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-05-16 10:34:09 +0600

    client req from email(query) and token(header) sended

--- index.js
+++ index.js
@@ -43,6 +43,8 @@ async function run() {
 
         app.get('/booking', async (req, res) => {
             const patient = req.query.patient;
+            const authorization = req.headers.authorization;
+            console.log('auth header: ',authorization);
             const query = { patient: patient };
             const bookings = await bookingCollection.find(query).toArray();
             res.send(bookings);
@@ -96,7 +98,8 @@ async function run() {
                 $set: user,
             };
             const result = await usersCollection.updateOne(filter, updateDoc, options);
-            res.send(result);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ result, token });
         })
 
 
