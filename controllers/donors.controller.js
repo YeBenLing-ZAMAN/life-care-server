520bed4c3a2ca59ed087082cf9f9ceff6b5b65ed
md kamruzzaman <kamruzzaman@triplewsols.org>
2023-04-02 11:16:18 +0600

    donors controller is updated

--- controllers/donors.controller.js
+++ controllers/donors.controller.js
@@ -1,19 +1,18 @@
-const User = require("../Models/User.model.js");
+const User = require("../models/UserSchema.js");
 
 const getDonors = async (req, res) => {
-  let filter = { role: "donor" };
-  if (Object.keys(req.query)?.length) {
-    const { bloodGroup, location, date, eligibility } = req.query;
-    const bloodGroupRegex = new RegExp(bloodGroup, "i");
-    const locationRegex = new RegExp(location, "i");
-
-    filter.bloodGroup = bloodGroupRegex;
-    filter.location = locationRegex;
-  }
-
   try {
-    const donors = await User.find(filter);
+    let filter = { role: "donor" };
+    if (Object.keys(req.query)?.length) {
+      const { bloodGroup, location, date, eligibility } = req.query;
+      const bloodGroupRegex = new RegExp(bloodGroup, "i");
+      const locationRegex = new RegExp(location, "i");
 
+      filter.bloodGroup = bloodGroupRegex;
+      filter.location = locationRegex;
+    }
+
+    const donors = await User.find(filter);
     res.status(200).json(donors);
   } catch (error) {
     console.log(error);
@@ -25,11 +24,10 @@ const getDonor = async (req, res) => {
   const { donorId } = req.params;
 
   if (!donorId.match(/^[0-9a-fA-F]{24}$/))
-    return res.json({ message: "No profile found" });
+    return res.status(400).json({ message: "No profile found" });
 
   try {
     const donor = await User.findOne({ _id: donorId });
-
     res.status(200).json({ donor });
   } catch (error) {
     console.log(error);
